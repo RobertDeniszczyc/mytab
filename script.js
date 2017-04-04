e5f92fdf4bd2cc9f975c852a0aa27031a21c3bee
Luke Todd <luke.todd@plus.net>
2017-04-04 16:45:30 +0100

    Clock ticks and buttons small

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-/* Inject the time and date into the page (client set) */
-
-var currentDate = new Date;
-
-var currentTimeContainer = document.getElementById('current-date');
-currentTimeContainer.textContent = currentDate.toDateString();
-
-var currentTimeContainer = document.getElementById('current-time');
-var currentMinutes = ('0'+currentDate.getMinutes()).slice(-2);
-currentTimeContainer.textContent = currentDate.getHours() + ":" + currentMinutes;
-
-
 /* https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript */
 function loadJSON(callback) {   
 
     var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
+    xobj.overrideMimeType("application/json");
     xobj.open('GET', 'links.json', true);
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
@@ -24,7 +12,7 @@ function loadJSON(callback) {
     xobj.send(null); 
  }
 
- function parseJSON() {
+function parseJSON() {
     loadJSON(function(response) {
       // Parse JSON string into object
       var linksJSON = JSON.parse(response);
@@ -32,48 +20,61 @@ function loadJSON(callback) {
     });
 }
 
-function loadColumns(linksJSON) {
-    var links = linksJSON;
-    var columnOne = links.column1;
+function updateClock() {
+    const currentDate = new Date;
+    const currentMinutes = ('0'+currentDate.getMinutes()).slice(-2);
+    const currentDateContainer = document.getElementById('current-date');
+    const currentTimeContainer = document.getElementById('current-time');
+
+
+    currentDateContainer.textContent = currentDate.toDateString();
+    currentTimeContainer.innerHTML = `${currentDate.getHours()}<span class="ticker">:</span>${currentMinutes}`;
+}
 
 function walk(links) {
     var columnsContainer = document.getElementById('columns-container');
-    for (var key in links) {
+    Object.keys(links).map((key, index) => {
         if (links.hasOwnProperty(key)) {
-          var column = links[key];
-          var columnHTML = '<div class=\"link-container\" id=\"column_' + key + '\"><div class=\"column-heading-container clearfix\"><h1 class=\"column-heading echo\">' + key + '</h1></div></div>'
-          columnsContainer.innerHTML += columnHTML;
+            var column = links[key];
+            var columnHTML = 
+                `<div class="link-container" id="column_${key}">
+                    <div class="column-heading-container clearfix">
+                        <h1 class="column-heading echo">${key}</h1>
+                    </div>
+                </div>`;
+            columnsContainer.innerHTML += columnHTML;
+            let thisColumn = document.getElementById(`column_${key}`);
 
-          var thisColumn = document.getElementById('column_' + key);
-          for (var link in column) {
-            if (column.hasOwnProperty(link)) {
-                var link = column[link],
-                    linkId = key + link.title.replace(/\s/g,'');
-                var linkHTML = '<a href=\"' + link.url + '\">' +
-                '<div class=\"link clearfix\" id=\"' + linkId + '\"> ' +
-                '<div class=\"link-info\"><p class=\"link-title\">' + link.title + '</p> ' +
-                '<p class=\"link-url\">' + link.url + '</p></div></div></a>' +
-
-                /* We need to set the color of the before element so we're using scoped CSS 
-                    despite support almost being non-existant for scoped, browsers will allow
-                    us to include style tags outside of the head.*/
-                    
-                '<style type=\"text/css\" scoped>' +
-                '#' + linkId + ':before {' +
-                'background-color:' + link.colour + '; }' +
-                '</style>';
-                thisColumn.innerHTML += linkHTML;
-            }
-          }
-
+            Object.keys(column).map((key, index) => {
+                if (column.hasOwnProperty(key)) {
+                    let link = column[key];
+                    let linkId = key + link.title.replace(/\s/, '');
+                    let linkHTML = 
+                        `<a href="${link.url}">
+                            <div class="link clearfix" id="${linkId}">
+                                <div class="link-info">
+                                    <p class="link-title">${link.title}</p>
+                                </div>
+                            </div>
+                        </a>
+                        <style type="text/css" scoped>
+                            #${linkId}:before {
+                                background-color: ${link.colour};
+                            }
+                        </style>`;
+                    thisColumn.innerHTML += linkHTML;
+                }
+            })
         }
-    }
+    })
 }
-walk(links);
 
-    
+function loadColumns(linksJSON) {
+    var links = linksJSON;
+    var columnOne = links.column1;
+    walk(links);
 }
 
 parseJSON();
  
-
+setInterval(updateClock, 1000);
